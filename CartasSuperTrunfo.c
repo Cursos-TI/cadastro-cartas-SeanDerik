871752939416dabd39caee3fd9b79063ba7ffa9b
seanderik <153625345+SeanDerik@users.noreply.github.com>
2025-10-01 11:35:53 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,61 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char estado;
+  char[] codigoCarta;
+  char[] nomeCidade;
+  int populacao;
+  float area;
+  float pib;
+  int pontosTuristicos;
+  
   // Área para entrada de dados
+    printf("carta 1 - insira a letra do estado: ");
+  scanf("%c",&estado);
 
+    printf("insira a letra do estado: ");
+  scanf("%f",&codigo);
+
+    printf("insira a letra do estado: ");
+  scanf("%c",&nomeCidade);
+
+    printf("insira a letra do estado: ");
+  scanf("%c",&populacao);
+
+    printf("insira a letra do estado: ");
+  scanf("%d",&area);
+
+    printf("insira a letra do estado: ");
+  scanf("%f",&pib);
+
+    printf("insira a letra do estado: ");
+  scanf("%d",&pontosTuristicos);
+
+//separador
+
+      printf("carta 2 - insira a letra do estado: ");
+  scanf("%c",&estado);
+
+    printf("insira a letra do estado: ");
+  scanf("%f",&codigo);
+
+    printf("insira a letra do estado: ");
+  scanf("%c",&nomeCidade);
+
+    printf("insira a letra do estado: ");
+  scanf("%c",&populacao);
+
+    printf("insira a letra do estado: ");
+  scanf("%d",&area);
+
+    printf("insira a letra do estado: ");
+  scanf("%f",&pib);
+
+    printf("insira a letra do estado: ");
+  scanf("%d",&pontosTuristicos);
+  
   // Área para exibição dos dados da cidade
-
+  
+  
 return 0;
 } 
